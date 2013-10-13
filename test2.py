29cb951ac79b5b4fcfd35dc5a0bba843fce9c81e
imrePurret <imre.purret@gmail.com>
2013-10-13 22:55:50 +0300

    Additions to movement

--- test2.py
+++ test2.py
@@ -14,7 +14,6 @@ from datetime import datetime
 def leia(image, lower,higher):#leian roboti kontuurjooned, mille jargi saab keskkoha teada
     global koord
     imgthreshold=cv2.inRange(image.copy(),np.uint8(lower),np.uint8(higher))
-    #cv2.imshow("cam-test",imgthreshold)
     contours,hierarchy=cv2.findContours(imgthreshold,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     b = len(contours)
 
@@ -26,96 +25,94 @@ def leia(image, lower,higher):#leian roboti kontuurjooned, mille jargi saab kesk
 	x =(np.sum(longestElement, axis = 0)[0])/len(longestElement)
         koord.append(x)
         if koord[0][1]>int(float(sys.argv[2])):
-	    otse(ser1,ser2,'0')
-	    otse(ser1,ser3,'0')
-	    ser1.write('fs1\n')
-	    ser2.write('fs1\n')
-	    ser3.write('fs1\n')
+            allSpeedsToZero()
             sys.exit()
-        return koord
+            return True
+        return False
     
 def punkt_kaugus(a1,a2,b1,b2):#funktsioon kahe punkti kauguse leidmiseks
-        return (math.sqrt((a1-b1)**2 +(a2-b2)**2))    
+        return (math.sqrt((a1-b1)**2 +(a2-b2)**2))
 
-def nega(a):#et mu deviations list ei laheks negatiivseks ega ule lubatud piiride
-    for i in range(3):
-        if a[i]<0: a[i]=0
-        if a[i]>255: a[i]=255
-    return a
+def allSpeedsToZero():
+    ser1.write('sd0\n')
+    ser2.write('sd0\n')
+    ser3.write('sd0\n')
+    ser1.write('fs1\n')
+    ser2.write('fs1\n')
+    ser3.write('fs1\n')
 
 def drive(threadName, delay):
     while True:
     	if koord[0][0]==0:
-	    vasakule(ser2,ser3,'10')
-	    t1 = 0.1*(int(180/koord[0][1]) + int(5-abs(koord[0][0])/60))
+	    #setSpeed(ser2,ser3,'10')
+	    t1 = 0.05*(int(90/koord[0][1]) + int(9-abs(koord[0][0])/30))
 	    time.sleep(t1)
-	    vasakule(ser2,ser3,'0')
+	    setSpeed(ser2,ser3,'0')
 	elif koord[0][0]>350:
 	    print 'paremale'
-	    paremale(ser1,ser3,'10')
-	    t1 = 0.1*(int(180/koord[0][1]) + int(5-abs(koord[0][0]-320)/60))
+	    setSpeed(ser1,ser3,'10')
+	    t1 = 0.05*(int(90/koord[0][1]) + int(abs(koord[0][0]-320)/30))
 	    time.sleep(t1)
-	    paremale(ser1,ser3,'0')
+	    setSpeed(ser1,ser3,'0')
 	elif 290>koord[0][0]:
 	    print 'vasakule'
-	    vasakule(ser2,ser3,'10')
-	    t1 = 0.1*(int(180/koord[0][1]) + int(5-abs(koord[0][0])/60))
+	    setSpeed(ser2,ser3,'10')
+	    t1 = 0.05*(int(90/koord[0][1]) + int(9-abs(koord[0][0])/30))
 	    time.sleep(t1)
-	    vasakule(ser2,ser3,'0')
+	    setSpeed(ser2,ser3,'0')
         else:
 	    print 'otse'
 	    if koord[0][1]>150:
-		otse(ser1,ser2, '13')
+		setSpeed(ser1,ser2, '13')
             elif koord[0][1]>200:
-                otse(ser1,ser2, '12')
+                setSpeed(ser1,ser2, '12')
             elif koord[0][1]>250:
-                otse(ser1,ser2, '10')
+                setSpeed(ser1,ser2, '10')
 	    elif koord[0][1]>300:
-		otse(ser1,ser2,'9')
+		setSpeed(ser1,ser2,'9')
 	    else:
-		otse(ser1,ser2, '15')
-	    t1 = 0.1*(1+int(float(sys.argv[2])/koord[0][1]))
+		setSpeed(ser1,ser2, '15')
+	    t1 = 0.1*(1+int(float(sys.argv[2])/(koord[0][1]+30)))
 	    time.sleep(t1)
 	    otse(ser1,ser2, '0')
 
-def otse(ser1,ser2, dist):
-	ser1.write('sd'+dist+'\n')
-	ser2.write('sd'+dist+'\n')
-
-def vasakule(ser2,ser3):
-	ser2.write('sd'+str(dist)+'\n')
-	ser3.write('sd-'+str(dist)+'\n')
-
-def paremale(ser1,ser3):
-	ser1.write('sd'+str(dist)+'\n')
-        ser3.write('sd-'+str(dist)+'\n')
-
+def setSpeed(ser_1,ser_2, dist):
+	ser_1.write('sd'+dist+'\n')
+	ser_2.write('sd'+dist+'\n')
+	
 ser1 = serial.Serial('/dev/ttyACM2')
 ser1.write('?\n')
+ser1.write('fs0\n')
 ser2 = serial.Serial('/dev/ttyACM0')
 ser2.write('?\n')
+ser2.write('fs0\n')
 ser3 = serial.Serial('/dev/ttyACM1')
 ser3.write('?\n')
-ser1.write('fs0\n')
-ser2.write('fs0\n')
 ser3.write('fs0\n')
 
+#Yellow goal
+#Red - 234
+#Green - 205
+#Blue - 15
 
-varv_b = int(float(25))
-varv_g = int(float(55))
-varv_r = int(float(193))
-r_dev_b = int(float(20))
-r_dev_g = int(float(30))
-r_dev_r = int(float(40))
+#Blue goal
+#Red - 34
+#Green - 43
+#Blue - 110
 
-lower = []
-higher= []
+#Ball
+#Red - 193
+#Green - 55
+#Blue - 25
 
-lower.append(varv_b-r_dev_b), lower.append(varv_g-r_dev_g), lower.append(varv_r-r_dev_r)
-higher.append(varv_b+r_dev_b), higher.append(varv_g+r_dev_g), higher.append(varv_r+r_dev_r)
+ora_lower = [5,25,153]
+ora_higher = [45,85,233]
 
-lower = nega(lower)
-higher = nega(higher)
+blu_lower = [90,13,0]
+blu_higher = [130,73,74]
+
+yel_lower = [0,175,194]
+yel_higher = [35,235,255]
 
 cam = cv2.VideoCapture(int(float(sys.argv[1])))   # 0 -> index of camera
 loendur = 0
@@ -135,14 +132,16 @@ while True:
         start = datetime.now()#fps esimese kaadri jaoks
     if loendur == 5:
 	drive_thread = thread.start_new_thread( drive, ("Thread-1", 2))        
-    ch = cv2.waitKey(5)
+    ch = cv2.waitKey(50)
     loendur+=1
     
     s, img = cam.read()
-    leia(img,lower, higher)
+    if leia(img,ora_lower, ora_higher):
+        drive_thread.stop()
+        break
     cv2.circle(img,(koord[0][0], koord[0][1]), 15, (255,255,255), 25)#ringjoon keskkoha jaoks
 
-    kiri = "Koordinaat: "
+'''    kiri = "Koordinaat: "
     number = [koord[0][1],koord[0][0]]#siit edasi tulevad tesktid, mida kuvada
     tekst2 = str(number)
     kolmas = str(kiri+tekst2)
@@ -168,17 +167,39 @@ while True:
     cv2.putText(img,aeg_kokku , (5,45), cv2.FONT_HERSHEY_PLAIN, 1.0, [255,0,0], 1)#fps ekraanile
     
     cv2.imshow("cam-test",img)
-    number1 = number
+    number1 = number'''
     
     if ch == 27:
-        ch = cv2.waitKey(5)
+        ch = cv2.waitKey(50)
         drive_thread.stop()
-	otse(ser1,ser2,'0')
-	otse(ser1,ser3,'0')
-	ser1.write('fs1\n')
-	ser2.write('fs1\n')
-	ser3.write('fs1\n')
+        allSpeedsToZero()
         sys.exit()
         break
+
+koord = [(0,0)]
+number1 = [0,0]
+kiirus_kesk = 0
+
+while True:
+    if l == 0:
+        start = datetime.now()
+    if loendur == 5:
+	drive_thread = thread.start_new_thread( drive, ("Thread-1", 2))        
+    ch = cv2.waitKey(50)
+    loendur+=1
+    
+    s, img = cam.read()
+    if leia(img, blu_lower, blu_higher):
+        drive_thread.stop()
+        break
+    cv2.circle(img,(koord[0][0], koord[0][1]), 15, (255,255,255), 25)#ringjoon keskkoha jaoks
+
+    if ch == 27:
+        ch = cv2.waitKey(50)
+        drive_thread.stop()
+        allSpeedsToZero()
+        sys.exit()
+        break
+    
  
 cv2.destroyAllWindows()
